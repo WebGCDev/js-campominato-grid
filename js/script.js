5d6c83d6e00ff6903e88bb74c71ba7869d5e9802
WebGCDev <gaetano.chi@gmail.com>
2023-11-15 18:14:19 +0100

    feat: add Fix ciclo For & Generatore quadrati

--- js/script.js
+++ js/script.js
@@ -2,13 +2,27 @@ const container = document.querySelector('.container');
 
 reset();
 
-console.log(container);
+for (let i = 1; i <= 100; i++) {
+  const square = genSquare(i);
 
-for (let i = 0; i < 100; i++) {
-  const square = document.createElement('div');
-  square.className = 'square';
+  // Clicca sul quadrato e con metodo stampato in console ci permetterà di vedere quante volte clicchiamo su un determinato quadrato con relativo counter
+  square.addEventListener('click', function () {
+    console.log(this.id);
+    this.classList.toggle('checked');
+  });
+
   container.append(square);
 }
+
+// Generatore di quadrati
+function genSquare(index) {
+  const newSquare = document.createElement('div');
+  newSquare.className = 'square';
+  newSquare.id = index;
+  return newSquare;
+}
+
+// Reset della funzione
 
 function reset() {
   container.innerHTML = '';
